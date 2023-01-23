e53c2b8fc0f1cbfc3d9f9f56ca070c7e5b5be10f
krippix <krippix@users.noreply.github.com>
2023-01-24 00:09:29 +0100

    Finished implementation of 'add_' in database

--- Gustelfy/objects/playlist.py
+++ Gustelfy/objects/playlist.py
@@ -10,6 +10,8 @@ class Playlist(spotifyObject.SpotifyObject):
     owner_id: str
     managed: bool
     tracks = []
+    description: str
+    image_url: str
     
     def __init__(self, id: str, name: str, owner_id: str, tracks=[], managed=False, timestamp=int(time.time())):
         self.set_id(id)
@@ -26,7 +28,19 @@ class Playlist(spotifyObject.SpotifyObject):
         return self.owner_id
 
     def is_managed() -> bool:
-        return self.managed
+        if self.managed:
+            return 1
+        else:
+            return 0
+
+    def get_tracks() -> list[track.Track]:
+        return self.tracks
+
+    def get_description() -> str:
+        return self.description
+
+    def get_image_url() -> str:
+        return self.image_url
 
     # ---- Setter Functions ----
 
@@ -38,6 +52,12 @@ class Playlist(spotifyObject.SpotifyObject):
 
     def set_managed(self, is_managed: bool):
         self.managed = is_managed
+
+    def set_description(self, description: str):
+        self.description = description
+
+    def set_image_url(self, url: str):
+        self.image_url = url
 
     # ---- Other Functions ----
 
