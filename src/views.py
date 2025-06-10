e82035b1550dac443979a8c4f661b5848edc0591
krippix <krippix@users.noreply.github.com>
2025-06-10 19:29:59 +0200

    test models

--- src/views.py
+++ src/views.py
@@ -2,7 +2,10 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
+from .spotify import SpotifyCredentials
+
 
 def index(request: HttpRequest):
     print(type(request.user.id))
+    print(SpotifyCredentials(1))
     return HttpResponse(request.user.id)
